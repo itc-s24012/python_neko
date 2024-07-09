73d7f6bacf7261e78db7abbab3493730045c4403
itc-s24012 <s24012@std.it-college.ac.jp>
2024-07-09 07:53:00 +0000

    2章終わり

---
+++ 2-10.py
@@ -0,0 +1,14 @@
+print(pow(2,3))
+print(pow(3,2))
+print(2,3,4)
+print(4,3,2)
+print(2,3,4,sep='/')
+print(2,3,4,sep='-',end='----')
+arg_vals = [2,3,4]
+print(*arg_vals)
+print(arg_vals)
+print(pow(*divmod(20,3)))
+sample_list = ['2','3','4']
+list(map(str,sample_list))
+print(sample_list)
+
---
+++ 2-13.py
@@ -0,0 +1,9 @@
+from random import randint
+print(randint(0,10))
+from random import randint, choice
+print(randint(0,10))
+print(choice(['a','b','c']))
+import random as rd
+print(rd.randint(0,10))
+from random import randint as rint
+print(rint(1,10))
